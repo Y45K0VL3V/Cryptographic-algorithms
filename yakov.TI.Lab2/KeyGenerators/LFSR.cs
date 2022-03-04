e5351e25d775b6dc21a47b40166995ce4dd55181
Y45K0VL3V <yakovlevvadim666@gmail.com>
2022-03-04 17:19:37 +0300

    LFSR key generator start development.

---
+++ yakov.TI.Lab2/KeyGenerators/LFSR.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yakov.TI.Lab2.KeyGenerators
+{
+    public class LFSR
+    {
+        public LFSR(byte registerLength, string polynom)
+        {
+            RegisterLength = registerLength;
+            Polynom = polynom;
+        }
+
+        public byte RegisterLength { get; set; }
+        public string Polynom { get; set; }
+    }
+}
