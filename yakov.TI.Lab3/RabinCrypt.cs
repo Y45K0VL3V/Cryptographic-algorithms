93ce322d81095eefa1178b74415c104610727663
Y45K0VL3V <yakovlevvadim666@gmail.com>
2022-03-26 22:50:55 +0300

    Start development of Rabin cryptosystem. Created project&set reference.

---
+++ yakov.TI.Lab3/RabinCrypt.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yakov.TI.Lab3
+{
+    public class RabinCrypt
+    {
+    }
+}
