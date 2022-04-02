05c9fb77b98ebb7eee1da808e494c5e297365646
Y45K0VL3V <yakovlevvadim666@gmail.com>
2022-04-02 03:12:12 +0300

    Created start of encrypt method. Added number prime check (Ferm realization).

--- yakov.TI.Lab3/RabinCrypt.cs
+++ yakov.TI.Lab3/RabinCrypt.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,20 @@ namespace yakov.TI.Lab3
 {
     public class RabinCrypt
     {
+        public byte[] Encrypt(BigInteger privateKeyQ, BigInteger privateKeyP, out BigInteger publicKey, byte[] inputData)
+        {
+            if (!RabinHelpMath.IsNumberPrime(privateKeyP) || !RabinHelpMath.IsNumberPrime(privateKeyQ))
+                throw new Exception("Not all keys are prime.");
+
+            publicKey = privateKeyP * privateKeyQ;
+            return Encrypt(publicKey);
+        }
+
+        public byte[] Encrypt(BigInteger publicKey, byte[] inputData)
+        {
+
+
+            //return
+        }
     }
 }
---
+++ yakov.TI.Lab3/RabinHelpMath.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yakov.TI.Lab3
+{
+    public static class RabinHelpMath
+    {
+        public static bool IsNumberPrime(BigInteger num)
+        {
+            for (int i = 2; i <= 100; i++)
+            {
+                var n = BigInteger.ModPow(i, num - 1, num);
+                if (n != 1) 
+                {
+                    return false;
+                }
+
+            }
+            return true;         
+        }
+    }
+}
