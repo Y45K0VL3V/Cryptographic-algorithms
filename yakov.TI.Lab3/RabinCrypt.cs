21fa2e8ac395d1a26ca40c7c34201822ce8fc46d
Y45K0VL3V <yakovlevvadim666@gmail.com>
2022-04-03 00:33:40 +0300

    Added Rabin's encryption.

--- yakov.TI.Lab3/RabinCrypt.cs
+++ yakov.TI.Lab3/RabinCrypt.cs
@@ -9,20 +9,32 @@ namespace yakov.TI.Lab3
 {
     public class RabinCrypt
     {
-        public byte[] Encrypt(BigInteger privateKeyQ, BigInteger privateKeyP, out BigInteger publicKey, byte[] inputData)
+        public byte[] Encrypt(BigInteger privateKeyQ, BigInteger privateKeyP, out BigInteger publicKeyN, BigInteger publicKeyB,  byte[] inputData)
         {
             if (!RabinHelpMath.IsNumberPrime(privateKeyP) || !RabinHelpMath.IsNumberPrime(privateKeyQ))
                 throw new Exception("Not all keys are prime.");
 
-            publicKey = privateKeyP * privateKeyQ;
-            return Encrypt(publicKey);
+            if (privateKeyP % 4 != 3 || privateKeyQ % 4 != 3)
+                throw new Exception("Not all private keys %4 == 3");
+
+            publicKeyN = privateKeyP * privateKeyQ;
+            return Encrypt(publicKeyN, publicKeyB, inputData);
         }
 
-        public byte[] Encrypt(BigInteger publicKey, byte[] inputData)
+        public byte[] Encrypt(BigInteger publicKeyN, BigInteger publicKeyB, byte[] inputData)
         {
+            if (publicKeyN >= publicKeyB)
+                throw new Exception("Public key B must be lower, than N");
 
+            byte[] outputData = new byte[publicKeyN.GetByteCount() * inputData.Length];
 
-            //return
+            for(int i = 0; i <= inputData.Length - 1; i++)
+            {
+                byte currByte = inputData[i];
+                (currByte * (currByte + publicKeyB) % publicKeyN).ToByteArray().CopyTo(outputData, i * publicKeyN.GetByteCount());
+            }
+
+            return outputData;
         }
     }
 }
