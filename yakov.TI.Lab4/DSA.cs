28b43ebe90a7c95f4fdfcca2eb79bded62f22921
Y45K0VL3V <yakovlevvadim666@gmail.com>
2022-05-18 02:43:18 +0300

    Created own struct for DSA params.

--- yakov.TI.Lab4/DSA.cs
+++ yakov.TI.Lab4/DSA.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace yakov.TI.Lab4
 {
+    public struct Params
+    {
+        public BigInteger q { get; set; }
+        public BigInteger p { get; set; }
+        public BigInteger h { get; set; }
+        public BigInteger x { get; set; }
+        public BigInteger k { get; set; }
+    }
+
     public class DSA
     {
+
+        public static byte[] ToSign(byte[] input, Params @params)
+        {
+
+            throw new NotImplementedException();
+        }
     }
 }
--- yakov.TI.Lab4/DSAParamsValidator.cs
+++ yakov.TI.Lab4/DSAParamsValidator.cs
@@ -10,21 +10,21 @@ namespace yakov.TI.Lab4
 {
     public static class DSAParamsValidator
     {
-        public static bool Validate(BigInteger q, BigInteger p, BigInteger h, BigInteger x, BigInteger k)
+        public static bool Validate(Params @params)
         {
-            if (!HelpMath.IsNumberPrime(q))
+            if (!HelpMath.IsNumberPrime(@params.q))
                 throw new ArgumentException("Q must be prime.");
 
-            if ((p - 1) % q != 0)
+            if ((@params.p - 1) % @params.q != 0)
                 throw new ArgumentException("Q must be divider of p-1.");
 
-            if ((h <= 1) || (h >= p - 1))
+            if ((@params.h <= 1) || (@params.h >= @params.p - 1))
                 throw new ArgumentException("H must be in range of [2, p-2]");
 
-            if ((x <= 0) || (x >= q))
+            if ((@params.x <= 0) || (@params.x >= @params.q))
                 throw new ArgumentException("X must be in range of [1, q-1]");
 
-            if ((k <= 0) || (k >= q))
+            if ((@params.k <= 0) || (@params.k >= @params.q))
                 throw new ArgumentException("K must be in range of [1, q-1]");
 
             return true;
