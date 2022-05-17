81828bdd3cdce8cee7fe015809052a8d5a5f15ab
Y45K0VL3V <yakovlevvadim666@gmail.com>
2022-05-18 02:10:02 +0300

    Added simple hash function.

--- yakov.TI.Lab4/DSA.cs
+++ yakov.TI.Lab4/DSA.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace yakov.TI.Lab4
 {
-    public class Class1
+    public class DSA
     {
     }
 }
---
+++ yakov.TI.Lab4/SimpleHash.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yakov.TI.Lab4
+{
+    public static class SimpleHash
+    {
+        public const int DEFAULT_HASH = 100;
+
+        public static string ToHash(byte[] input, BigInteger modNum)
+        {
+            try
+            {
+                BigInteger currHash = Convert.ToInt32(Math.Pow(DEFAULT_HASH + input[0], 2)) % modNum;
+
+                for (int i = 1; i < input.Length; i++)
+                    currHash = Convert.ToInt32(Math.Pow((double)currHash + input[i], 2)) % modNum;
+
+                return currHash.ToString();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
