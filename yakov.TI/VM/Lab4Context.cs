f9a409cb4107d23a43cac889b7d4c99eb7e0f1d9
Y45K0VL3V <yakovlevvadim666@gmail.com>
2022-05-18 05:15:48 +0300

    Set lab 4 UI classes.

---
+++ yakov.TI/VM/Lab4Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yakov.TI.VM
+{
+    public class Lab4Context : INotifyPropertyChanged
+    {
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
